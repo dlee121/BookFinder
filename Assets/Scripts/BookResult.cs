05a0643cfc33584b4fa04163544da892ab911839
dlee121 <dlee121@illinois.edu>
2016-09-22 23:29:45 -0500

    prototype

---
+++ Assets/Scripts/BookResult.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BookResult{
+
+	public string title { get; set; }
+	public string author { get; set; }
+	public string url { get; set; }
+	//add thumbnail which you can press to access corresponding info, like summaries, reviews, pricing, etc
+}
